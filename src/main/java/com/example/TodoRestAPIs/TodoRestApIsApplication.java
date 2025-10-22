4e334bbf7b2bb97040a4fe44bea9871836a80890
Ashish <ay0393101@gmail.com>
2025-10-22 22:01:50 +0530

    Added Basic Controller Structure for Todo RestAPIs using spring boot

---
+++ src/main/java/com/example/TodoRestAPIs/TodoRestApIsApplication.java
@@ -0,0 +1,13 @@
+package com.example.TodoRestAPIs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodoRestApIsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodoRestApIsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/TodoRestAPIs/TodoRestApIsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.TodoRestAPIs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoRestApIsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
